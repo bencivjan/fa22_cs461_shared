903f0ec95442f10e8c037a971cf2e51369a685b9
bencivjan <bcivjan2@illinois.edu>
2022-10-24 23:22:45 -0500

    Implement wha

--- Crypto/sol_3.1.6.py
+++ Crypto/sol_3.1.6.py
@@ -9,23 +9,24 @@
 #                         ((byte XOR 0x33) Left Shift 16) OR
 #                         ((byte XOR 0xAA) Left Shift 8) OR
 #                         (byte XOR 0x55)
-# outHash = (outHash AND Mask) + (intermediate_value AND Mask)
+#     outHash = (outHash AND Mask) + (intermediate_value AND Mask)
 # return outHash
 
 def WHA(inStr: str):
     outHash = 0
-    mask = int('0x3FFFFFFF', 16)
+    mask = 0x3FFFFFFF
 
     for char in inStr:
         byte = ord(char)
-        intermediate_value = (((byte ^ int('0xCC', 16)) << 24) |
-                             ((byte ^ int('0x33', 16)) << 16) |
-                             ((byte ^ int('0xAA', 16)) << 8) |
-                             ( byte ^ int('0x55', 16)))
-    outHash = (outHash & mask) + (intermediate_value & mask)
+        intermediate_value = (((byte ^ 0xCC) << 24) |
+                             ((byte ^ 0x33) << 16) |
+                             ((byte ^ 0xAA) << 8) |
+                             ( byte ^ 0x55))
+        outHash = (outHash & mask) + (intermediate_value & mask)
     return outHash
-print(WHA("Hello world!"))
-assert format(WHA("Hello world!"), '#x') == '0x50b027cf'
-assert format(WHA("I am Groot."), '#x') == '0x57293cbb'
+
+assert WHA(" ") == 0x2c138a75
+assert WHA("Hello world!") == 0x50b027cf
+assert WHA("I am Groot.") == 0x57293cbb
 
 print("Passed tests")
