ca0b06e2f67655a6781050f175cb4b7f1dafb2aa
Ben Civjan <bcivjan2@illinois.edu>
2022-11-01 19:22:08 -0500

    Finish 3.2.2

--- Crypto/sol_3.2.3.py
+++ Crypto/sol_3.2.3.py
@@ -1,4 +1,5 @@
 import urllib.request, urllib.error
+from math import ceil
 
 def get_status(u):
     try:
@@ -9,27 +10,34 @@ def get_status(u):
 
 url_base = 'http://172.22.159.75:8080/mp3/fa22_cs461_bcivjan2/?'
 ciphertext = ''
-with open('3.2.3_ciphertext.hex') as f:
+with open('/Users/bencivjan/Desktop/fa22_cs461_bcivjan2/Crypto/3.2.3_ciphertext.hex') as f:
     ciphertext = f.read().strip()
 
 # for block in range(1,2):#len(ciphertext)):
 text = ''
-pad = ''
-G = [0 for x in range(16)] # equal to 0x10 ^ i in order
-for byte in range(16):
-    print(G)
-    for i in range(256):
-        status = get_status(url_base + ciphertext[:-(16 + 2*(byte+1))] + format(i, '02x') + pad + ciphertext[-(16 + 2*byte - len(pad)):])
-        if status == 404:
-            pt = 0x10 ^ i ^ int(ciphertext[-(16 + 2*(byte+1)) : -(16 + 2*byte)], 16)
-            text = chr(pt) + text
-            G[-(byte+1)] = 0x10 ^ i
-            print(status, format(i, '02x'))
+
+for block in range(1, ceil(len(ciphertext) / 32)+1):
     pad = ''
-    for j in range(16, 15-byte, -1):
-        pad = format(j ^ G[j-1], '02x') + pad
-        # else:
-        #     print(status)
+    G = [0 for x in range(16)] # equal to 0x10 ^ i in order
+
+    for byte in range(16):
+        print(G)
+        pad = ''
+        xor_val = 0xf
+        for j in range(16-byte, 16):
+            pad += format(xor_val ^ G[j], '02x')
+            xor_val -= 1
+
+        for i in range(256):
+            end_idx = -(32*(block-1)) if block-1 > 0 else 99999999
+            ct_mod = ciphertext[:-(32*block + 2*(byte+1))] + format(i, '02x') + pad + ciphertext[-(32*block + 2*byte - len(pad)) : end_idx]
+            status = get_status(url_base + ct_mod)
+            if status == 404 or (byte == 9 and status == 0 and block == 1):
+                pt = 0x10 ^ i ^ int(ciphertext[-(32*block + 2*(byte+1)) : -(32*block + 2*byte)], 16)
+                text = chr(pt) + text
+                G[-(byte+1)] = 0x10 ^ i
+                print(status, format(i, '02x'))
+
 print(text)
 
 get_status(f'http://172.22.159.75:8080/mp3/fa22_cs461_bcivjan2/?{ciphertext}')
